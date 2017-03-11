64437a87df3d0a9e4bd97de9930a2de7391a15da
Sergey Lavrov <bizywiz@gmail.com>
2017-03-11 08:30:10 +0300

    added models and migrations

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return super(QuestionManager, self).get_query_set().order_by('added_at')
+
+    def popular(self):
+        return super(QuestionManager, self).get_query_set().annotate(Count('likes')).order_by('likes__count')
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes =  models.ManyToManyField(User, related_name='likes_set')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
